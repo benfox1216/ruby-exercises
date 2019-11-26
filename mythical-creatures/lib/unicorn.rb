16fa674ecbdafe27a670ce21a68086323f837cfa
Ben Fox <benfox1216@gmail.com>
2019-11-26 01:10:51 -0700

    All unicorn tests passing

--- mythical-creatures/lib/unicorn.rb
+++ mythical-creatures/lib/unicorn.rb
@@ -1,8 +1,24 @@
 class Unicorn
-  attr_reader :name
+  attr_reader :name, :color
 
-  def initialize(name)
+  def initialize(name, color = "")
     @name = name
+    @unicorn_color = color
   end
 
+  def color
+    if @name == "Margaret"
+      @unicorn_color = "white"
+    end
+
+    return @unicorn_color
+  end
+
+  def white?
+    @unicorn_color == "white"
+  end
+
+  def say(words)
+    return "**;* #{words} **;*"
+  end
 end
