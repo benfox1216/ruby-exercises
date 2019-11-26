3f30edf2c81bbea4d43283613188b2a72e975511
Ben Fox <benfox1216@gmail.com>
2019-11-26 02:16:04 -0700

    All dragon tests passing

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -1,10 +1,23 @@
 class Dragon
-  attr_reader :name, :color, :rider
+  attr_reader :name, :color, :rider, :full, :is_hungry
 
   def initialize(name, color, rider)
     @name = name
     @color = color
     @rider = rider
+    @full = 0
+    @is_hungry = true
   end
 
+  def hungry?
+    @is_hungry == true
+  end
+
+  def eat
+    @full += 1
+
+    if @full == 3
+      @is_hungry = false
+    end
+  end
 end
