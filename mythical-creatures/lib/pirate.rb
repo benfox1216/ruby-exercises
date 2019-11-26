6480e804582b7ebfda8d920fd13aa117564ad24f
Ben Fox <benfox1216@gmail.com>
2019-11-26 12:13:42 -0700

    All pirate tests passing

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -1,8 +1,22 @@
 class Pirate
-  attr_reader :name
+  attr_reader :name, :job, :heinous_acts, :booty
 
-  def initialize(name)
+  def initialize(name, job = "Scallywag", heinous_acts = 0, booty = 0)
     @name = name
+    @job = job
+    @heinous_acts = heinous_acts
+    @booty = booty
   end
 
+  def cursed?
+    @heinous_acts > 2
+  end
+
+  def commit_heinous_act
+    @heinous_acts +=1
+  end
+
+  def rob_ship
+    @booty += 100
+  end
 end
