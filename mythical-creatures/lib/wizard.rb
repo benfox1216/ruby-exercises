886c20342a8b2f64f5ed480cf9b0492db9dc52a3
Ben Fox <benfox1216@gmail.com>
2019-11-26 13:17:02 -0700

    All wizard tests passing

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -1,8 +1,27 @@
 class Wizard
-  attr_reader :name
+  attr_reader :name, :bearded, :root_powers, :spells_cast
 
-  def initialize(name)
+  def initialize(name, bearded = true, root_powers = "sudo", spells_cast = 0)
     @name = name
+    @bearded = bearded
+    @root_powers = root_powers
+    @spells_cast = spells_cast
   end
 
+  def bearded?
+    @bearded == true
+  end
+
+  def incantation(add_powers)
+    @root_powers += " " + add_powers
+  end
+
+  def rested?
+    true if spells_cast < 3
+  end
+
+  def cast
+    @spells_cast += 1
+    "MAGIC MISSILE!"
+  end
 end
