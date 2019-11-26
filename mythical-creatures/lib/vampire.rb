74b82bd68d744b77e0d443c990294443f7947aeb
Ben Fox <benfox1216@gmail.com>
2019-11-26 01:59:47 -0700

    All vampire tests passing

--- mythical-creatures/lib/vampire.rb
+++ mythical-creatures/lib/vampire.rb
@@ -1,8 +1,17 @@
 class Vampire
-  attr_reader :name
+  attr_reader :name, :pet, :is_thirsty
 
-  def initialize(name)
+  def initialize(name, pet = "bat", is_thirsty = true)
     @name = name
+    @pet = pet
+    @is_thirsty = is_thirsty
   end
 
+  def thirsty?
+    @is_thirsty == true
+  end
+
+  def drink
+    @is_thirsty = false
+  end
 end
