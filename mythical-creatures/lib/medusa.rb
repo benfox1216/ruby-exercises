bdccc77d81eca9dd66597673cc346201545e0fee
Ben Fox <benfox1216@gmail.com>
2019-11-26 23:25:53 -0700

    All medusa tests passing

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -1,13 +1,21 @@
 class Medusa
-  attr_reader :name, :statues
+  attr_reader :name, :statues, :number_stoned
 
-  def initialize(name_parameter)
+  def initialize(name_parameter, number_stoned = 0)
     @name = name_parameter
     @statues = []
+    @number_stoned = number_stoned
   end
 
   def stare(person)
-    statues << person
-    person.stoned = true
+    if @statues.count < 3
+      person.is_stoned = true
+      @statues << person
+    else
+      @statues[0].is_stoned = false
+      @statues.shift
+      @statues << person
+      @statues[2].is_stoned = true
+    end
   end
 end
--- mythical-creatures/lib/person.rb
+++ mythical-creatures/lib/person.rb
@@ -1,7 +1,13 @@
 class Person
+  attr_reader :name
+  attr_accessor :is_stoned
 
-  def initialize
-
+  def initialize(name, is_stoned = false)
+    @name = name
+    @is_stoned = is_stoned
   end
 
+  def stoned?
+    @is_stoned == true
+  end
 end
