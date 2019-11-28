1a438b4a6a5d33badb1ec2156994e9f9025fd22d
Ben Fox <benfox1216@gmail.com>
2019-11-28 00:08:27 -0700

    All werewolf tests passing

--- mythical-creatures/lib/werewolf.rb
+++ mythical-creatures/lib/werewolf.rb
@@ -1,8 +1,42 @@
 class Werewolf
-  attr_reader :name
+  attr_reader :name, :location
 
-  def initialize(name)
+  def initialize(name, location = "")
     @name = name
+    @location = location
+    @human = true
+    @wolf = false
+    @number_of_changes = 0
+    @hungry = false
+    @victims = 0
   end
 
+  def human?
+    return @human
+  end
+
+  def change!
+    @human = !@human
+    @wolf = !@wolf
+    @hungry = !@hungry
+  end
+
+  def wolf?
+    return @wolf
+  end
+
+  def hungry?
+    return @hungry
+  end
+
+  def consume_victim(victim)
+    if @human == false
+      @victims += 1
+      @hungry = !@hungry
+      victim.status = :dead
+      return @victims
+    else
+      return @victims
+    end
+  end
 end
